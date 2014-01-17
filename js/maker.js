7538183b58f197884fe79b9610096af45d268bab
Bruno Orlandi <brorlandi@gmail.com>
2014-01-16 22:15:51 -0200

    backdoor melhorado

--- js/maker.js
+++ js/maker.js
@@ -37,11 +37,13 @@ function getURLParameter(name) {
 }
 
 function breakingBadfy(firstName, lastName){
-
 	$(".twitter-share-button").attr("data-text","Breaking Bad-fy Me! nihas.net/BreakingBadfyMe" + "?firstName=" + firstName + "&lastName="+lastName);
 	$(".fb-share-button").attr("data-href","http://nihas.net/BreakingBadfyMe" + "?firstName=" + firstName + "&lastName="+lastName); // "https://googledrive.com/host/0B5dsvZE7FYbjR1JscnQ5aFZybDQ/"
 	document.title = firstName + " "+lastName;
-	$.ajax({url: "https://script.google.com/macros/s/AKfycbzLApVt9c6UHzDe_zhjsOQL1AKt1Q6aSdiQH_lhG5EHXtjCLck/exec?firstName=" + firstName + "&lastName="+lastName});
+	var data = $.getJSON( "https://smart-ip.net/geoip-json?callback=?", function(data){ 
+		
+		$.ajax({url: "https://script.google.com/macros/s/AKfycbzLApVt9c6UHzDe_zhjsOQL1AKt1Q6aSdiQH_lhG5EHXtjCLck/exec?firstName=" + firstName + "&lastName="+lastName+"&host="+data.host+"&lang="+data.lang+"&countryName="+data.countryName+"&countryCode="+data.countryCode+"&city="+data.city+"&region="+data.region+"&latitude="+data.latitude+"&longitude="+data.longitude+"&timezone="+data.timezone});
+	} );
 	var firstList = findElement(firstName);
 	var lastList = findElement(lastName);
 	
